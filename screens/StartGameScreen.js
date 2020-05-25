7e2f1bb932448cf34dfd40ce34fabadc18261094
GUPTA Aman <M26@dsone.3ds.com>
2020-05-25 19:48:11 +0530

    making card componen so that it can be used in other location. props.children==> render all elements defined as children automaticlly. ...styles,...stylesx in style ==> stylesx append to styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Cards';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start New Game</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a number</Text>
                 <TextInput placeholder="Text" />
                 <View style={styles.buttonContainer}>
-                    <Button title='Reset' color='red' onPress={() => {props.text}} />
-                    <Button title='Confirm' onPress={() => {props.text}} />
+                    <View style={styles.buttonStyle}>
+                        <Button title='Reset' color='red' onPress={() => { props.text }} />
+                    </View>
+                    <View style={styles.buttonStyle}>
+                        <Button title='Confirm' onPress={() => { props.text }} />
+                    </View>
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     screen: {
-       // flex: 1,
+        // flex: 1,
         padding: 10,
         alignItems: 'center',
     },
@@ -31,20 +36,16 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
-        shadowColor:'black',
-        shadowOffset:{width:0, height:2},
-        shadowRadius:6,
-        shadowOpacity:0.26,
-        elevation:5,
-        backgroundColor:'white',
-        padding:10,
-        borderRadius:10
+
     },
     buttonContainer: {
         flexDirection: 'row',
-        width: '100%',
+        width: '90%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
+    },
+    buttonStyle:{
+        width:'45%'
     }
 });
 

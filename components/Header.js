742d0d319a3d0df08e4c3fb8434919e3ab33ff4c
GUPTA Aman <M26@dsone.3ds.com>
2020-05-25 19:17:48 +0530

    showdow works with ios, elivation work with android. these are to give card effects

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 
-const Header = props =>{
-return (<View style={headerStyles.headerView}>
-    <Text style={headerStyles.headerText} >
-        {props.title}
-    </Text>
-</View>
-)};
+const Header = props => {
+    return (<View style={headerStyles.headerView}>
+        <Text style={headerStyles.headerText} >
+            {props.title}
+        </Text>
+    </View>
+    )
+};
 
 const headerStyles = StyleSheet.create({
-headerView:{
-width:'100%',
-height:90,
-paddingTop:36,
-backgroundColor:'#f7287b',
-alignItems:'center',
-justifyContent:'center'
-},
-headerText :{
-color:'black',
-fontSize:18
-}
+    headerView: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        color: 'black',
+        fontSize: 18
+    }
 });
 
 export default Header;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Screen!</Text>
+            <Text style={styles.title}>Start New Game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a number</Text>
+                <TextInput placeholder="Text" />
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset' color='red' onPress={() => {props.text}} />
+                    <Button title='Confirm' onPress={() => {props.text}} />
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
+       // flex: 1,
         padding: 10,
         alignItems: 'center',
-        justifyContent: 'flex-start'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor:'black',
+        shadowOffset:{width:0, height:2},
+        shadowRadius:6,
+        shadowOpacity:0.26,
+        elevation:5,
+        backgroundColor:'white',
+        padding:10,
+        borderRadius:10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 });
 

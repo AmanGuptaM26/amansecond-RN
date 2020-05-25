59dbf99cd3b08612bca2e3015a6d17defc5282ea
GUPTA Aman <M26@dsone.3ds.com>
2020-05-25 22:26:15 +0530

    use of let variable. show conditional message

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import Card from '../components/Cards';
 import Colors from '../constants/Colors';
 import Input from '../components/Input';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
-
+    const [confirmed, setConfirmed]= useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
+           
     const numberInputHandler = (inputValue) => {
         //if(value)
         //return;
         console.log(inputValue);
         setEnteredValue(inputValue.replace(/[^0-9]/g, ''));
     };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+    };
+
+    const confirmInputHandler = () => {
+        const choosenNumber =parseInt(enteredValue);
+        if(choosenNumber === NaN || choosenNumber<=0 || choosenNumber>999){
+        //Alert.alert('Invalid Number!')
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(choosenNumber);
+        setEnteredValue('');
+    };
+    
+    let confirmedOutput;
+
+    if(confirmed)
+    {
+    confirmedOutput =<Text>Choosen Number: {selectedNumber}</Text>
+    }
 
     return (
         <TouchableWithoutFeedback onPress={()=>{
@@ -23,7 +47,7 @@ const StartGameScreen = props => {
                 <Card style={styles.inputContainer}>
                     <Text>Select a number</Text>
                     <Input
-                        placeholder="Test"
+                      //  placeholder="Test"
                         maxLength={3}
                         keyboardType="number-pad"
                         autoCorrect={false}
@@ -34,13 +58,14 @@ const StartGameScreen = props => {
                     ></Input>
                     <View style={styles.buttonContainer}>
                         <View style={styles.buttonStyle}>
-                            <Button title='Reset' color={Colors.redbutton} onPress={() => { props.text }} />
+                            <Button title='Reset' color={Colors.redbutton} onPress={ resetInputHandler } />
                         </View>
                         <View style={styles.buttonStyle}>
-                            <Button title='Confirm' color={Colors.bluebutton} onPress={() => { props.text }} />
+                            <Button title='Confirm' color={Colors.bluebutton} onPress={confirmInputHandler} />
                         </View>
                     </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     );

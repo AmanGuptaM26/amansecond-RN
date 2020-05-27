767c07d20a7947d8e012e552cd3d8ec6348d5799
GUPTA Aman <M26@dsone.3ds.com>
2020-05-27 14:02:02 +0530

    setting up screena nd number conatiner

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,12 +3,13 @@ import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Ke
 import Card from '../components/Cards';
 import Colors from '../constants/Colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
-    const [confirmed, setConfirmed]= useState(false);
+    const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState();
-           
+
     const numberInputHandler = (inputValue) => {
         //if(value)
         //return;
@@ -18,28 +19,45 @@ const StartGameScreen = props => {
 
     const resetInputHandler = () => {
         setEnteredValue('');
+        setConfirmed(false);
+        setSelectedNumber();
+        Keyboard.dismiss();
     };
 
     const confirmInputHandler = () => {
-        const choosenNumber =parseInt(enteredValue);
-        if(choosenNumber === NaN || choosenNumber<=0 || choosenNumber>999){
-        //Alert.alert('Invalid Number!')
+        const choosenNumber = parseInt(enteredValue);
+        if (isNaN(choosenNumber) || choosenNumber <= 0 || choosenNumber > 999) {
+            Alert.alert('Invalid Number!', 'Choose between 1 to 999 only',
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]);
             return;
         }
         setConfirmed(true);
         setSelectedNumber(choosenNumber);
         setEnteredValue('');
+        Keyboard.dismiss();
     };
     
+const startGameHandler =() =>{
+Alert.alert('So ja moti bains!!!','Bains hoke ullo ki jase kyu jagti hai raat ko',
+[{text:'Okay main bains hun',style:'destructive', onPress:resetInputHandler}])
+};
+
     let confirmedOutput;
 
-    if(confirmed)
-    {
-    confirmedOutput =<Text>Choosen Number: {selectedNumber}</Text>
+    if (confirmed) {
+        confirmedOutput = (
+            <Card style={styles.summaryCard}>
+                <Text>
+                    Choosen Number
+                </Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="Start Game" onPress={startGameHandler} />
+            </Card>
+        );
     }
 
     return (
-        <TouchableWithoutFeedback onPress={()=>{
+        <TouchableWithoutFeedback onPress={() => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
@@ -47,7 +65,7 @@ const StartGameScreen = props => {
                 <Card style={styles.inputContainer}>
                     <Text>Select a number</Text>
                     <Input
-                      //  placeholder="Test"
+                        //  placeholder="Test"
                         maxLength={3}
                         keyboardType="number-pad"
                         autoCorrect={false}
@@ -58,7 +76,7 @@ const StartGameScreen = props => {
                     ></Input>
                     <View style={styles.buttonContainer}>
                         <View style={styles.buttonStyle}>
-                            <Button title='Reset' color={Colors.redbutton} onPress={ resetInputHandler } />
+                            <Button title='Reset' color={Colors.redbutton} onPress={resetInputHandler} />
                         </View>
                         <View style={styles.buttonStyle}>
                             <Button title='Confirm' color={Colors.bluebutton} onPress={confirmInputHandler} />
@@ -95,6 +113,10 @@ const styles = StyleSheet.create({
     },
     buttonStyle: {
         width: '45%'
+    },
+    summaryCard: {
+        marginTop: 20,
+        alignItems:"center"
     }
 });
 
